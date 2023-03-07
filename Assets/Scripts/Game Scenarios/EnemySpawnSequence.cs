58870cfd3cc5a73433d7e79e6899d5ad405c1bb8
Mehmet Berkay Coruk <89832806+MehmetBerkayC@users.noreply.github.com>
2023-03-07 04:06:21 +0300

    Working on Enemy waves

---
+++ Assets/Scripts/Game Scenarios/EnemySpawnSequence.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySpawnSequence 
+{
+    [SerializeField]
+    EnemyFactory[] factories = default;
+
+    [SerializeField]
+    EnemyType type = EnemyType.Medium;
+
+    [SerializeField, Range(1, 100)]
+    int amount = 1;
+
+    [SerializeField, Range(0.1f, 10f)]
+    float cooldown = 1f;
+
+    public State Begin() => new State(this);
+
+    //To make the state survive hot reloads in the editor it needs to be serializable.
+    // Downside of this approach is that we need to create a new state object each time a sequence is started
+    // avoid memory allocations by making it as a struct instead of a class
+    [System.Serializable] 
+    public struct State // Nested
+    {
+        EnemySpawnSequence sequence;
+
+        public State(EnemySpawnSequence sequence)
+        {
+            this.sequence = sequence;
+        }
+    }
+}
